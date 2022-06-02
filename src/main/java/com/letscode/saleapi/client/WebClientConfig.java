fa9546a37feb13426cead17046ffebf2a38c5133
rosanper <rp.santana117@gmail.com>
2022-06-01 21:07:12 -0300

    refactor: uri clients

--- src/main/java/com/letscode/saleapi/client/WebClientConfig.java
+++ src/main/java/com/letscode/saleapi/client/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.letscode.saleapi.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -9,10 +10,20 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Component
 public class WebClientConfig {
 
+    @Value("${spring.data.viacep.uri}")
+    private String cepClientUri;
+
+    @Value("${spring.data.user-api.uri}")
+    private String userClientUri;
+
+    @Value("${spring.data.product-api.uri}")
+    private String productClientUri;
+
+
     @Bean
     public WebClient webClientCep(WebClient.Builder builder){
         return builder
-                .baseUrl("viacep.com.br/ws/")
+                .baseUrl(cepClientUri)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
@@ -20,7 +31,7 @@ public class WebClientConfig {
     @Bean
     public WebClient webClientUser(WebClient.Builder builder){
         return builder
-                .baseUrl("http://localhost:8082")
+                .baseUrl(userClientUri)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
@@ -28,7 +39,7 @@ public class WebClientConfig {
     @Bean
     public WebClient webClientProduct(WebClient.Builder builder){
         return builder
-                .baseUrl("http://localhost:8080")
+                .baseUrl(productClientUri)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
