3733815773268747a3c5200f34e39f7ce007f4bb
rosanper <rp.santana117@gmail.com>
2022-06-02 12:00:08 -0300

    refactor: mongo uri

--- src/main/java/com/letscode/saleapi/config/MongoConfig.java
+++ src/main/java/com/letscode/saleapi/config/MongoConfig.java
@@ -2,11 +2,16 @@ package com.letscode.saleapi.config;
 
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 
 @Configuration
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
+
+    @Value("${spring.data.mongo.uri}")
+    private String mongoUri;
+
     @Override
     protected String getDatabaseName() {
         return "sales";
